1afb8b85fde6dbb71729519af31b49398b6bb675
Parker Ly <Parker_Ly@mckinsey.com>
2021-11-30 14:27:29 +0700

    add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
 import Login from './Login';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <Login></Login>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
