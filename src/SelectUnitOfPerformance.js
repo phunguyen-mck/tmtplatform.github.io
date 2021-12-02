d63649e27e83c5526af8bb59b4d2a4d7d8bba87a
Parker Ly <Parker_Ly@mckinsey.com>
2021-12-02 17:27:03 +0700

    finish unit of performance cards

--- src/SelectUnitOfPerformance.js
+++ src/SelectUnitOfPerformance.js
@@ -1,6 +1,6 @@
 import './mck-bootstrap.min.css';
 import React from 'react';
-import styled from '@emotion/styled';
+import styled from '@emotion/styled/macro';
 
 import {
   Container,
@@ -9,21 +9,72 @@ import {
   ThemeProvider,
   TYPE_OUTLINE,
 } from '@mds/mds-reactjs-library';
-import ArrowLeft from '@mds/mds-icons/icons/svg/outline-16-arrow-left.svg';
-import { useNavigate } from 'react-router-dom';
+import ArrowLeft from '@mds/mds-icons/icons/svg/outline-48-arrow-left.svg';
+import Account from './images/account.svg';
+import Deals from './images/deals.svg';
+import ServiceLines from './images/service-lines.svg';
+import Alliances from './images/alliances.svg';
+import Org from './images/org.svg';
+import CapabilityBuilding from './images/capability-building.svg';
 
 const UnitOfPerformanceCard = function (props) {
+  const IconContainer = styled.div`
+    height: 100px;
+    width: 100px;
+    background-color: #2251ff;
+    border-radius: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #ffffff;
+    margin: 29px 0 29px 18px;
+  `;
+
   const Container = styled.div`
     display: flex;
+    height: 156px;
+    width: 300px;
+    background-color: #e2f3ff;
+    border-radius: 8px;
+    align-items: center;
+    color: #333333;
+
+    & + * {
+      margin-left: 28px;
+    }
+
+    :hover {
+      background: #2251ff;
+      color: #ffffff;
+
+      ${IconContainer} {
+        background-color: #5478fe;
+        border-radius: 8px 200px 200px 8px;
+        height: 200px;
+        width: 200px;
+        margin: 0 0 0 -82px;
+        padding-left: 50px;
+        transition: all 300ms;
+      }
+
+      transition: all 200ms;
+    }
+
+    overflow: hidden;
   `;
-  const IconContainer = styled.div`
-    display: flex;
+
+  const UnitNameDiv = styled.div`
+    font: normal normal 500 24px/26px McKinsey Sans;
+    letter-spacing: 0.58px;
+    margin-left: 18px;
+    max-width: 150px;
   `;
   return (
     <Container>
       <IconContainer>
-        <Icon size={16} type={TYPE_OUTLINE} src={ArrowLeft} />
+        <Icon size={48} type={TYPE_OUTLINE} src={props.iconSrc} />
       </IconContainer>
+      <UnitNameDiv>{props.name}</UnitNameDiv>
     </Container>
   );
 };
@@ -34,14 +85,26 @@ const SelectUnitOfPerformance = function () {
       <Container>
         <Grid container className="pt-sm-6 px-sm-2 px-md-4">
           <Grid item span={12}>
-            <div className="d-flex flex-column">
+            <div className="d-flex flex-column align-content-center">
               <div>
                 <Icon size={16} type={TYPE_OUTLINE} src={ArrowLeft} />
               </div>
               <div>Select units of performance</div>
-              <div>
-                <UnitOfPerformanceCard name={'Account'} />
-                <div></div>
+              <div className="d-flex flex-row">
+                <UnitOfPerformanceCard name={'Account'} iconSrc={Account} />
+                <UnitOfPerformanceCard name={'Deals'} iconSrc={Deals} />
+                <UnitOfPerformanceCard
+                  name={'Service Lines'}
+                  iconSrc={ServiceLines}
+                />
+              </div>
+              <div className="d-flex flex-row mt-4">
+                <UnitOfPerformanceCard name={'Alliances'} iconSrc={Alliances} />
+                <UnitOfPerformanceCard name={'Org and people'} iconSrc={Org} />
+                <UnitOfPerformanceCard
+                  name={'Capability building'}
+                  iconSrc={CapabilityBuilding}
+                />
               </div>
             </div>
           </Grid>
