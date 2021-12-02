64874b471e668ec0850d38b5b6f9b46d76d76e90
Nam Tran <nam_tran@mckinsey.com>
2021-12-02 16:49:50 +0700

    Implement ITSpendInfo

--- src/components/Chart/DoughnutChart/DoughnutLegend.jsx
+++ src/components/Chart/DoughnutChart/DoughnutLegend.jsx
@@ -8,23 +8,29 @@ import {
 } from '@mds/mds-reactjs-library';
 
 const StyledColorBox = styled.div`
-  width: 14px;
+  min-width: 14px;
   height: 14px;
   background: ${(props) => props.backgroundColor};
   margin-right: 8px;
+  margin-top: 2px;
+`;
+
+const StyledContentWrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
 `;
 
 const DoughnutLegend = ({ chartData }) => {
   const { labels, backgroundColors } = chartData;
   return (
-    <Grid container spacing={20} className="w-100 mt-4">
+    <Grid container spacing={16} className="w-100 mt-4 ml-2">
       {labels.map((label, index) => {
         return (
-          <Grid item span={6}>
-            <div className="d-inline-flex align-items-flex-start">
+          <Grid item span={6} className="p-0 pb-2">
+            <StyledContentWrapper className="d-flex">
               <StyledColorBox backgroundColor={backgroundColors[index]} />
               <Typography type={BODY_SMALL}>{label}</Typography>
-            </div>
+            </StyledContentWrapper>
           </Grid>
         );
       })}
